e8edfd77131607fb288046a7d3963ed53967fb64
joyrehema17-gif <joyrehema17@gmail.com>
2026-07-06 17:32:52 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,35 @@
+// ==========================================
+// HERO IMAGE SLIDER
+// ==========================================
 
+const slides = document.querySelectorAll(".slide");
+
+if (slides.length > 0) {
+
+    let currentSlide = 0;
+
+    function showSlide(index) {
+
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
+
+        slides[index].classList.add("active");
+    }
+
+    function nextSlide() {
+
+        currentSlide++;
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+
+        showSlide(currentSlide);
+    }
+
+    showSlide(currentSlide);
+
+    setInterval(nextSlide, 5000);
+
+}
