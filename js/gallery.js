4b522c63754858b7e450cf9048e88666894e3620
joyrehema17-gif <joyrehema17@gmail.com>
2026-07-07 07:59:42 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,37 @@
+const galleryImages = document.querySelectorAll(".gallery-item img");
 
+const galleryModal = document.getElementById("galleryModal");
+
+const galleryImage = document.getElementById("galleryImage");
+
+const galleryClose = galleryModal.querySelector(".close");
+
+galleryImages.forEach(img => {
+
+    img.onclick = function(){
+
+        galleryModal.style.display = "block";
+
+        galleryImage.src = this.src;
+
+        galleryImage.alt = this.alt;
+
+    }
+
+});
+
+galleryClose.onclick = function(){
+
+    galleryModal.style.display = "none";
+
+}
+
+galleryModal.onclick = function(e){
+
+    if(e.target === galleryModal){
+
+        galleryModal.style.display = "none";
+
+    }
+
+}
