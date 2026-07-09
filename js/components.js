4b5354d08468329604a8d164a8aa20c8d06a8669
joyrehema17-gif <joyrehema17@gmail.com>
2026-07-09 21:28:21 +0300

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,13 +1,25 @@
 async function loadComponent(id, file) {
 
     const response = await fetch(file);
-
     const html = await response.text();
 
     document.getElementById(id).innerHTML = html;
 
+    if (id === "navbar") {
+
+        const currentPage = location.pathname.split("/").pop() || "index.html";
+
+        document.querySelectorAll(".nav-links a").forEach(link => {
+
+            if (link.getAttribute("href") === currentPage) {
+                link.classList.add("active");
+            }
+
+        });
+
+    }
+
 }
 
 loadComponent("navbar", "components/navbar.html");
-
 loadComponent("footer", "components/footer.html");
